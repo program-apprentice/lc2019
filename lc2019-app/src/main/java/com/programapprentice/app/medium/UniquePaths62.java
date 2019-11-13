1e23fe2313ee5d031f011435def9d5e1898d309f
program-apprentice <rui.jiang.tech@gmail.com>
2019-11-12 23:08:34 -0800

    Add solution for 62 Unique Path

--- lc2019-app/src/main/java/com/programapprentice/app/medium/UniquePaths62.java
+++ lc2019-app/src/main/java/com/programapprentice/app/medium/UniquePaths62.java
@@ -1,4 +1,17 @@
 package com.programapprentice.app.medium;
 
 public class UniquePaths62 {
+  public int uniquePaths(int height, int width) {
+    int[] opt = new int[width];
+    opt[0] = 1;
+    for(int row = 0; row < height; row++) {
+      for(int col = 0; col < width; col++) {
+        if (col != 0) {
+          opt[col] += opt[col - 1];
+        }
+      }
+    }
+    return opt[width-1];
+  }
+
 }
