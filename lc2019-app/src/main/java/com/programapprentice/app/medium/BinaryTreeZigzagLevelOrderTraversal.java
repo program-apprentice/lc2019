11c0b289f7f9a541f0eba4f539b73776bfac586c
program-apprentice <rui.jiang.tech@gmail.com>
2019-11-13 10:25:28 -0800

    Add another solution for 103 Binary Tree Zigzag Level Order Traversal

--- lc2019-app/src/main/java/com/programapprentice/app/medium/BinaryTreeZigzagLevelOrderTraversal.java
+++ lc2019-app/src/main/java/com/programapprentice/app/medium/BinaryTreeZigzagLevelOrderTraversal.java
@@ -42,4 +42,38 @@ public class BinaryTreeZigzagLevelOrderTraversal {
     traverse(root.left, result, level++);
     traverse(root.right, result, level++);
   }
+
+  public List<List<Integer>> zigzagLevelOrderNonRecursive(TreeNode root) {
+    List<List<Integer>> result = new ArrayList<>();
+    if (root == null) {
+      return  result;
+    }
+
+    boolean isEvenLevel = false;
+    List<TreeNode> curLevel = new ArrayList<>();
+    curLevel.add(root);
+
+    while(!curLevel.isEmpty()) {
+      List<Integer> level = new ArrayList<>();
+      List<TreeNode> nextLevel = new ArrayList<>();
+      for(TreeNode node : curLevel) {
+        if (isEvenLevel) {
+          level.add(0, node.val);
+        } else {
+          level.add(node.val);
+        }
+        if (node.left != null) {
+          nextLevel.add(node.left);
+        }
+        if (node.right != null) {
+          nextLevel.add(node.right);
+        }
+      }
+      result.add(level);
+      isEvenLevel = !isEvenLevel;
+      curLevel = nextLevel;
+    }
+
+    return result;
+  }
 }
