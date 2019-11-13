935b9f5efde9073cd892fc8aab559ff7e53da8aa
Rui Jiang <rui.jiang.dev@gmail.com>
2019-11-12 16:43:06 -0800

    Add solution for 229 Majority Element II

---
+++ lc2019-app/src/main/java/com/programapprentice/app/MajorityElementII229.java
@@ -0,0 +1,48 @@
+package com.programapprentice.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MajorityElementII229 {
+  public List<Integer> majorityElement(int[] nums) {
+    List<Integer> result = new ArrayList<>();
+    int candidate1 = 0;
+    int candidate2 = 0;
+    int counter1 = 0;
+    int counter2 = 0;
+
+    for(int num : nums) {
+      if (num == candidate1) {
+        counter1++;
+      } else  if (num == candidate2) {
+        counter2++;
+      } else if (counter1 == 0) {
+        counter1 = 1;
+        candidate1 = num;
+      } else if (counter2 == 0) {
+        counter2 = 1;
+        candidate2 = num;
+      } else {
+        counter1--;
+        counter2--;
+      }
+    }
+    counter1 = 0;
+    counter2 = 0;
+
+    for (int num : nums) {
+      if (num == candidate1) {
+        counter1++;
+      } else if (num == candidate2) {
+        counter2++;
+      }
+    }
+    if (counter1 > (nums.length/3)) {
+      result.add(candidate1);
+    }
+    if (counter2 > (nums.length/3)) {
+      result.add(candidate2);
+    }
+    return result;
+  }
+}
