f7d329fd3658bf962c7039a10083e4d96c009daf
program-apprentice <rui.jiang.tech@gmail.com>
2020-02-01 20:43:17 -0800

    Add solution for 753 Cracking The Safe

---
+++ lc2019-app/src/test/java/com/programapprentice/app/hard/CrackingTheSafe753_T.java
@@ -0,0 +1,12 @@
+package com.programapprentice.app.hard;
+
+import org.junit.Test;
+
+public class CrackingTheSafe753_T {
+  CrackingTheSafe753 solution = new CrackingTheSafe753();
+
+  @Test
+  public void test1() {
+    System.out.println(solution.crackSafe(2,2));
+  }
+}
